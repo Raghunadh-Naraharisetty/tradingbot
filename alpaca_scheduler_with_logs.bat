60606449a335a5ebf8cd6b80fb90cf16fbab4ca0
Raghunadh Naraharisetty <49860399+Raghunadh-Naraharisetty@users.noreply.github.com>
2026-02-17 09:59:47 -0500

    Upgrade: 5 indicators + news + earnings + confidence scoring

--- alpaca_scheduler_with_logs.bat
+++ alpaca_scheduler_with_logs.bat
@@ -9,7 +9,7 @@ set logfile=logs\bot_%date:~-4,4%%date:~-10,2%%date:~-7,2%.log
 
 :: Run and log everything
 echo Starting Trading Bot - %date% %time% > %logfile%
-python alpaca_scheduler.py --interval -5m >> %logfile% 2>&1
+python alpaca_scheduler.py --interval 5m --position 2000
 
 :: python alpaca_scheduler.py --interval -15m >> %logfile% 2>&1
 

83c661712ee343da9b09cbd7eb2a694541808f6a
Raghunadh Naraharisetty <49860399+Raghunadh-Naraharisetty@users.noreply.github.com>
2026-02-17 01:36:47 -0500

    Add Alpaca integration and new signal dashboard

--- alpaca_scheduler_with_logs.bat
+++ alpaca_scheduler_with_logs.bat
@@ -9,6 +9,6 @@ set logfile=logs\bot_%date:~-4,4%%date:~-10,2%%date:~-7,2%.log
 
 :: Run and log everything
 echo Starting Trading Bot - %date% %time% > %logfile%
-python alpaca_scheduler.py --interval -15m >> %logfile% 2>&1
+python alpaca_scheduler.py --interval -5m >> %logfile% 2>&1
 
 echo Bot stopped - %date% %time% >> %logfile%
